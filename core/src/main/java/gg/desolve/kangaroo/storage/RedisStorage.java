3c5a7f5becef31a78151896df79082dbbb423574
Michele <michelegje@gmail.com>
2026-04-21 04:34:02 +0100

    Include pub & sub to redis store

--- core/src/main/java/gg/desolve/kangaroo/storage/RedisStorage.java
+++ core/src/main/java/gg/desolve/kangaroo/storage/RedisStorage.java
@@ -1,25 +1,33 @@
 package gg.desolve.kangaroo.storage;
 
-import lombok.Getter;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.JedisPoolConfig;
+import redis.clients.jedis.JedisPubSub;
 
 import java.net.URI;
+import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 import java.util.function.Function;
 
 public class RedisStorage {
 
-    @Getter
     private final JedisPool pool;
 
     public RedisStorage(String uri) {
         JedisPoolConfig config = new JedisPoolConfig();
         config.setMaxTotal(16);
         config.setMaxIdle(8);
+        config.setTestOnBorrow(false);
+        config.setBlockWhenExhausted(true);
 
-        this.pool = new JedisPool(config, URI.create(uri));
+        this.pool = new JedisPool(
+                config,
+                URI.create(uri),
+                10_000,
+                10_000
+        );
+
     }
 
     public void execute(Consumer<Jedis> action) {
@@ -34,7 +42,45 @@ public class RedisStorage {
         }
     }
 
+    public void publish(String channel, String message) {
+        execute(jedis -> jedis.publish(channel, message));
+    }
+
+    public Subscription subscribe(String channel, BiConsumer<String, String> listener) {
+        JedisPubSub pubSub = new JedisPubSub() {
+            @Override
+            public void onMessage(String ch, String message) {
+                listener.accept(ch, message);
+            }
+        };
+
+        Thread thread = new Thread(() -> {
+            try (Jedis jedis = pool.getResource()) {
+                jedis.subscribe(pubSub, channel);
+            } catch (Exception e) {
+                if (!Thread.currentThread().isInterrupted()) {
+                    e.printStackTrace();
+                }
+            }
+        }, "kangaroo-sub-" + channel);
+        thread.setDaemon(true);
+        thread.start();
+
+        return () -> {
+            try {
+                pubSub.unsubscribe();
+            } catch (Exception ignored) {
+            }
+            thread.interrupt();
+        };
+    }
+
     public void close() {
         pool.close();
+    }
+
+    @FunctionalInterface
+    public interface Subscription {
+        void unsubscribe();
     }
 }
